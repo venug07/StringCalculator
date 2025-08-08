a4a3ffa9c73eb623ab614ed70e626b3b10a28246
venug07 <venug078175@yahoo.com>
2025-08-08 20:44:48 +0530

    Changes done in method to return 0 for empty values

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,13 +1,17 @@
 class StringCalculator
   
    def add(string_numbers="")
-         @stringnumbers = string_numbers.split ","
+       @stringnumbers = string_numbers.split ","
+       if !@stringnumbers.empty?
          if @stringnumbers.length > 1
           @stringnumbers = @stringnumbers.inject {|acu, v| acu.to_i + v.to_i }
          else
           @stringnumbers.first.to_i
          end
          return @stringnumbers
+      else
+       return 0
+      end  
    end
 
 end
