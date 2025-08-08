c204a096a5fa4168a230ccdf8c94353ed32764c2
venug07 <venug078175@yahoo.com>
2025-08-08 20:52:20 +0530

    Changes done in method for raising exception for negative values

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,14 +1,22 @@
 class StringCalculator
   
    def add(string_numbers="")
+       neg_values = []
        @stringnumbers = string_numbers.split ","
        if !@stringnumbers.empty?
-         if @stringnumbers.length > 1
-          @stringnumbers = @stringnumbers.inject {|acu, v| acu.to_i + v.to_i }
-         else
-          @stringnumbers.first.to_i
+         @stringnumbers.each do |sn|
+           neg_values << sn.to_i if sn.to_i < 0
          end
+        if neg_values.empty?
+          if @stringnumbers.length > 1
+           @stringnumbers = @stringnumbers.inject {|acu, v| acu.to_i + v.to_i }
+          else
+           @stringnumbers.first.to_i
+          end
          return @stringnumbers
+        else
+         raise Exception.new "Negative numbers not allowed " + neg_values.join(", ")
+        end
       else
        return 0
       end  
